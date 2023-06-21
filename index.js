5d85b56d759885eb8bcc001f13179b0b8b311861
Emerson Fully <63175980+emersonfully@users.noreply.github.com>
2023-06-21 12:40:20 -0300

    add cors to fix local request

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const path = require('path');
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 // Set up in-memory SQLite database
 const db = new sqlite3.Database(':memory:');
 
